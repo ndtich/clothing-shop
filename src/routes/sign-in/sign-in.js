47c5c685d7de5b7d84943dec285f121c92a97766
ndtich <ndtich@gmail.com>
2022-04-30 16:02:32 +0700

    Setup basic firebase and authentication

---
+++ src/routes/sign-in/sign-in.js
@@ -0,0 +1,20 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase';
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(userDocRef);
+  };
+  return (
+    <div>
+      <h1>Sign in</h1>
+      <button onClick={logGoogleUser}>Sign in with google popup</button>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,49 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDfqeFJV2vW2p-g_WdWMGOSpoc2vPPTKkI',
+  authDomain: 'clothing-shop-db-c2ec5.firebaseapp.com',
+  projectId: 'clothing-shop-db-c2ec5',
+  storageBucket: 'clothing-shop-db-c2ec5.appspot.com',
+  messagingSenderId: '538951620445',
+  appId: '1:538951620445:web:0407e724d3aa75fbd67ab4',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+  return userDocRef;
+};
